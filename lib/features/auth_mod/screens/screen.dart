c37fa10e3380f0dd888baa6482f946ae19be7858
Shibaji Debnath <imshibaji@gmail.com>
2022-05-02 20:42:15 +0530

    Auth Features Updated

---
+++ lib/features/auth_mod/screens/screen.dart
@@ -0,0 +1,6 @@
+export './desktop/login_desktop_screen.dart';
+export './mobile/forget_mobile_screen.dart';
+export './mobile/login_mobile_screen.dart';
+export './mobile/profile_mobile.screen.dart';
+export './mobile/register_mobile_screen.dart';
+export './tablet/login_tablet_screen.dart';
