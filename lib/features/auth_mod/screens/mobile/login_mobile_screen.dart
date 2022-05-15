7754fcd14e108fb42d8d496ef4ce8674b349efc1
Shibaji Debnath <imshibaji@gmail.com>
2022-05-15 19:38:52 +0530

    Auth UI Update for Mobile

--- lib/features/auth_mod/screens/mobile/login_mobile_screen.dart
+++ lib/features/auth_mod/screens/mobile/login_mobile_screen.dart
@@ -2,7 +2,7 @@ import 'dart:developer';
 
 import 'package:clean_archetructure/core/helpers/login_helper.dart';
 import 'package:clean_archetructure/core/widgets/day_night_switch.dart';
-import 'package:clean_archetructure/features/auth_mod/widgets/text_input_field.dart';
+import 'package:clean_archetructure/features/auth_mod/widgets/auth_button.dart';
 import 'package:clean_archetructure/features/auth_mod/widgets/widget_functions.dart';
 import 'package:flutter/material.dart';
 
@@ -31,134 +31,92 @@ class _LoginMobileScreenState extends State<LoginMobileScreen> {
       ),
       body: Form(
         key: _globalKey,
-        child: ListView(
-          children: [
-            SizedBox(
-              height: MediaQuery.of(context).size.height / 3.8,
-            ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListView(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height / 5.5,
+              ),
 
-            const Center(
-              child: Text(
+              const Text(
                 'Login Area',
                 style: TextStyle(fontSize: 30),
+                textAlign: TextAlign.center,
               ),
-            ),
+              const SizedBox(
+                height: 5,
+              ),
 
-            // Username Fields
-            TextInputField(
-              prefixIcon: Icons.account_circle_outlined,
-              labelTextStr: 'Input Username',
-              validator: (user) {
-                if (user != null) {
-                  username = user;
-                  return null;
-                }
-                return 'Please Input your Username';
-              },
-            ),
-            // usernameField(
-            //   onValid: (user) => username = user,
-            // ),
+              // Username Fields
+              usernameField(
+                onValid: (user) => username = user,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
 
-            // Password Fields
-            TextInputField(
-              prefixIcon: Icons.security_outlined,
-              labelTextStr: 'Input Password',
-              validator: (pass) {
-                if (pass != null) {
-                  password = pass;
-                  return null;
-                }
-                return 'Please Input Your Password';
-              },
-              obscureText: _isPasswordHide,
-              suffixIcon: IconButton(
-                onPressed: () {
+              // Password Fields
+              passwordField(
+                onValid: (pass) => password = pass,
+                passHide: _isPasswordHide,
+                onKeyBtnPressed: (val) {
                   setState(() {
-                    _isPasswordHide = !_isPasswordHide;
+                    _isPasswordHide = val;
                   });
                 },
-                icon: Icon(
-                  _isPasswordHide
-                      ? Icons.visibility
-                      : Icons.visibility_off_outlined,
-                ),
               ),
-            ),
-            // passwordField(
-            //   onValid: (pass) => password = pass,
-            //   passHide: _isPasswordHide,
-            //   onKeyBtnPressed: (val) {
-            //     setState(() {
-            //       _isPasswordHide = val;
-            //     });
-            //   },
-            // ),
+              const SizedBox(
+                height: 25,
+              ),
 
-            // Buttons Fields
-            forgetButton(
-              context,
-              onForget: () {
-                return true;
-              },
-            ),
-            loginRegisterButtons(
-              context,
-              key: _globalKey,
-              onLogin: () {
-                log('username: $username, password: $password');
-                doAuth(context, username!, password!);
-                return true;
-              },
-              onRegister: () {
-                return true;
-              },
-            ),
-            const Divider(
-              thickness: 3,
-            ),
+              // Buttons Fields
+              forgetButton(
+                context,
+                onForget: () {
+                  return true;
+                },
+              ),
+              loginRegisterButtons(
+                context,
+                key: _globalKey,
+                onLogin: () {
+                  log('username: $username, password: $password');
+                  doAuth(context, username!, password!);
+                  return true;
+                },
+                onRegister: () {
+                  return true;
+                },
+              ),
+              const Divider(
+                thickness: 3,
+              ),
 
-            // Social Login Buttons
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  primary: const Color.fromARGB(255, 220, 70, 0),
-                ),
+              // Social Login Buttons
+              AuthButton(
+                label: 'Login by Facebook',
                 onPressed: () {},
-                child: const Text(
-                  'Login by Google',
-                  style: TextStyle(color: Colors.white),
-                ),
+                backgroundColor: const Color.fromARGB(255, 7, 108, 191),
+                textColor: Colors.white,
+                paddingValue: 15,
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  primary: const Color.fromARGB(255, 0, 60, 181),
-                ),
+              AuthButton(
+                label: 'Login by Google',
                 onPressed: () {},
-                child: const Text(
-                  'Login by Facebook',
-                  style: TextStyle(color: Colors.white),
-                ),
+                backgroundColor: const Color.fromARGB(255, 235, 53, 17),
+                textColor: Colors.white,
+                paddingValue: 15,
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  primary: const Color.fromARGB(255, 1, 158, 255),
-                ),
+              AuthButton(
+                label: 'Login by LinkedIn',
                 onPressed: () {},
-                child: const Text(
-                  'Login by LinkedIn',
-                  style: TextStyle(color: Colors.white),
-                ),
+                backgroundColor: const Color.fromARGB(255, 17, 126, 235),
+                textColor: Colors.white,
+                paddingValue: 15,
               ),
-            )
-          ],
+            ],
+          ),
         ),
       ),
     );
